cbef0e04eae0eda4e50e2befe3a14a526673cc22
CTeck <onglai0801@gmail.com>
2025-04-09 01:44:19 +0800

    dyingd

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Order from './pages/Order';
+import Footer from './components/Footer';
+import Login from './components/Login';
 import DoneOrder from './pages/DoneOrder';
 import Menu from './pages/Menu';
 import Faq from './pages/Faq';
-import Footer from './components/Footer';
-import Login from './components/Login';
 
 const App = () => {
-  const [showLogin, setShowLogin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false); // This state controls the login modal visibility
+  const location = useLocation();
 
   return (
-    <Router>
+    <>
       <div className='app'>
-        {/* Only show Login modal if showLogin is true */}
-        {showLogin && <Login setShowLogin={setShowLogin} />}
-        
+
+      {/* Only show Login modal if showLogin is true */}
+      {showLogin && <Login setShowLogin={setShowLogin} />} 
+
+        {/* Pass setShowLogin to Navbar */}
         <Navbar setShowLogin={setShowLogin} />
         
         <Routes>
@@ -28,13 +30,14 @@ const App = () => {
           <Route path='/Order' element={<Order />} />
           <Route path='/DoneOrder' element={<DoneOrder />} />
           <Route path='/Menu' element={<Menu />} />
-          <Route path='/Faq' element={<Faq />} />
-        </Routes>
+          <Route path='/Faq' element={<Faq/>} />
 
-        <Footer />
+        </Routes>
       </div>
-    </Router>
+
+      <Footer />
+    </>
   );
-};
+}
 
 export default App;
